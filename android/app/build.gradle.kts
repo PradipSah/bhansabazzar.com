f32c0e488264648a254d1cc78ce4a546f3be4c4b
Pradip Sah <82813600+PradipSah@users.noreply.github.com>
2026-06-29 23:42:32 +0545

    after error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.laligurans.ppsgtech"
+    namespace = "com.bhansabazzar.ppsgtech"
     compileSdk = 36
 
     ndkVersion = "28.2.13676358"
@@ -36,7 +36,7 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         multiDexEnabled = true
-        applicationId = "com.laligurans.ppsgtech"
+        applicationId = "com.bhansabazzar.ppsgtech"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
--- android/app/src/main/kotlin/com/u6amtech/sixvalley_ecommerce/MainActivity.kt
+++ android/app/src/main/kotlin/com/u6amtech/sixvalley_ecommerce/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.laligurans.ppsgtech
+package com.bhansabazzar.ppsgtech
 
 import io.flutter.embedding.android.FlutterActivity
 
